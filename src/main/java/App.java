bcd951d02aa39551be4ec0adbbe3423c464af4ef
voronkovam <m.voronka@mail.ru>
2023-07-14 12:25:54 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+
+        System.out.println(bean3 == bean4);
+
+
     }
 }
